d73a07d6ff3117f66c475940ded07bbd5b62a2c4
André Lins <andrelucas01@hotmail.com>
2020-09-18 08:42:49 -0300

    Inserting assets

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -5,18 +5,26 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Home()));
-          },
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/bg-login.png"),
+          fit: BoxFit.cover,
         ),
       ),
-    );
+      child: Row(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Open route'),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Home()));
+            },
+          )
+        ],
+      ),
+    ));
   }
 }
